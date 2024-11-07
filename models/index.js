34aac351913517235f5a4c0863d3cb836c2b8d9b
kadeem <kingkadeem11@gmail.com>
2024-11-07 00:43:22 -0500

    created all columns for models

--- models/index.js
+++ models/index.js
@@ -3,9 +3,12 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { ForeignKeyConstraintError } = require('../config/connection');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category,{
+  foreignKey: "category_id"
+})
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
